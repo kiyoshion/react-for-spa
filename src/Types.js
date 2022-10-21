923f89b17e2afafb9943af9955009d3d1a5f4097
kiyoshion <kiyoshion@gmail.com>
2022-10-21 12:47:49 +0900

    Add Types

---
+++ src/Types.js
@@ -0,0 +1,21 @@
+export const Types = {
+  ja: [
+      { id: 10, name: "動画", children:
+        [
+          { "id": 11, "name": "学習サービス" },
+          { "id": 12, "name": "海外ドラマ" },
+          { "id": 13, "name": "映画" },
+          { "id": 14, "name": "アニメ" },
+          { "id": 15, "name": "テレビ" }
+        ]
+      },
+     { id: 20, name: "本", children:
+      [
+        { "id": 21, "name": "参考書" },
+        { "id": 22, "name": "マンガ" },
+        { "id": 23, "name": "小説" },
+        { "id": 24, "name": "辞書" }
+      ]
+    }
+  ]
+}
