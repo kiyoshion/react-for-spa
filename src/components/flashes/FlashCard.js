b9353126b0ede239319fc9596ab3d9cdc9afc01a
kiyoshion <kiyoshion@gmail.com>
2022-10-21 12:54:28 +0900

    Update Flash

--- src/components/flashes/FlashCard.js
+++ src/components/flashes/FlashCard.js
@@ -1,22 +1,33 @@
 import { useEffect, useState } from 'react'
 import flashStyles from './FlashCard.module.scss'
 import ReactCardFlip from 'react-card-flip'
+import { CONSTS } from '../../Consts'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { setFlashModal } from '../../store/modalSlice'
 
 export default function FlashCard({flash}) {
-  const [ display, setDisplay ] = useState({
-    title: "", description: ""
-  })
   const [ isFlipped, setIsFlipped ] = useState(false)
+  const [ currentFlash, setCurrentFlash ] = useState({})
+  const flashModal = useSelector(state => state.modal.flashModal)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    setDisplay({ title: flash.front_title, description: flash.front_description })
+    setCurrentFlash(flash)
   }, [])
 
-  const speechTitle = (e) => {
+  const speechTitle = (e, side) => {
     e.stopPropagation()
-    const uttr = new SpeechSynthesisUtterance(display.title)
-        uttr.lang = "en-US"
-        speechSynthesis.speak(uttr)
+    if (side === 'front') {
+      const uttr = new SpeechSynthesisUtterance(flash.front_title)
+          uttr.lang = "en-US"
+          speechSynthesis.speak(uttr)
+    } else {
+      const uttr = new SpeechSynthesisUtterance(flash.back_title)
+          uttr.lang = "ja-JP"
+          speechSynthesis.speak(uttr)
+     }
   }
 
   const flipCard = (e) => {
@@ -24,27 +35,105 @@ export default function FlashCard({flash}) {
     setIsFlipped(!isFlipped)
   }
 
+  const handleFlashModal = (e, bool, side) => {
+    e.stopPropagation()
+    dispatch(setFlashModal({ isOpen: bool, side: side, flash: flash }))
+  }
+
+  const handleFlashModalFlip = (e, side, flash) => {
+    e.stopPropagation()
+    side === 'front' ?
+      dispatch(setFlashModal({ isOpen: true, side: 'back', flash: flash}))
+    :
+      dispatch(setFlashModal({ isOpen: true, side: 'front', flash: flash}))
+  }
+
   return (
+    <>
     <ReactCardFlip
       isFlipped={isFlipped}
       flipDirection="horizontal"
       flipSpeedBackToFront="0.3"
       flipSpeedFrontToBack="0.3"
     >
-      <div onClick={flipCard} className={flashStyles.card}>
-        <div
-          className={flashStyles.speechButton}
-          onClick={speechTitle}
-        >
-          <span>S</span>
+      <div onClick={flipCard} className={`${flashStyles.card}`}>
+        <div className={flashStyles.cardContent}>
+          <h2>{flash.front_title}</h2>
+          <p>{flash.front_description}</p>
         </div>
-        <h2>{flash.front_title}</h2>
-        <p>{flash.front_description}</p>
+        { flash.front_image_small && (
+          <div className={flashStyles.cardFooter}>
+            <div className={flashStyles.zoomContainer} onClick={(e) => handleFlashModal(e, true, 'front')}>
+              <img src={`${CONSTS.BACKEND_HOST_STORAGE}${flash.front_image_small}`} alt={flash.front_title} />
+            </div>
+            <div
+            className={flashStyles.speechButton}
+            onClick={(e) => speechTitle(e, 'front')}
+            >
+              <span>S</span>
+            </div>
+          </div>)
+        }
       </div>
       <div onClick={flipCard} className={`${flashStyles.card} ${flashStyles.back}`}>
-        <h2>{flash.back_title}</h2>
-        <p>{flash.back_description}</p>
+        <div className={flashStyles.cardContent}>
+          <h2>{flash.back_title}</h2>
+          <p>{flash.back_description}</p>
+        </div>
+        { flash.back_image_small && (
+          <div className={flashStyles.cardFooter}>
+            <div className={flashStyles.zoomContainer} onClick={(e) => handleFlashModal(e, true, 'back')}>
+              <img src={`${CONSTS.BACKEND_HOST_STORAGE}${flash.back_image_small}`} alt={flash.back_title} />
+            </div>
+            <div
+            className={flashStyles.speechButton}
+            onClick={(e) => speechTitle(e, 'back')}
+            >
+              <span>S</span>
+            </div>
+          </div>)
+        }
       </div>
     </ReactCardFlip>
+    { flashModal.isOpen &&
+      <div className={flashStyles.flashModalContainer}>
+        <div
+          className={flashStyles.flipButton}
+          onClick={(e) => handleFlashModalFlip(e, flashModal.side, flashModal.flash)}
+        >⇆
+        </div>
+        <div className={flashStyles.flashModalInner}>
+          { flashModal.side === 'front' ?
+            <>
+              <div className={flashStyles.flashModalHeader}>
+                <h2>{flashModal.flash.front_title}</h2>
+                {flashModal.flash.front_description &&
+                  <p>{flashModal.flash.front_description}</p>
+                }
+              </div>
+              { flashModal.flash.front_image_large &&
+                <img src={`${CONSTS.BACKEND_HOST_STORAGE}${flashModal.flash.front_image_large}`} alt={flashModal.flash.front_title} />
+              }
+            </>
+          :
+            <>
+              <div className={flashStyles.flashModalHeader}>
+                <h2>{flashModal.flash.back_title}</h2>
+                {flashModal.flash.back_description &&
+                  <p>{flashModal.flash.back_description}</p>
+                }
+              </div>
+              { flashModal.flash.back_image_large &&
+                <img src={`${CONSTS.BACKEND_HOST_STORAGE}${flashModal.flash.back_image_large}`} alt={flashModal.flash.back_title} />
+              }
+            </>
+          }
+        </div>
+        <div>
+          <button onClick={(e) => handleFlashModal(e, false, '')}>CLOSE</button>
+        </div>
+      </div>
+    }
+    </>
   )
 }
---
+++ src/components/modals/FlashModal.js
@@ -0,0 +1,7 @@
+
+
+export default function FlashModal(flash) {
+  return (
+    <div></div>
+  )
+}
