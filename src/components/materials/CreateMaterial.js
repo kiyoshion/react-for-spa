18f2ef985492fccc0bed2dc928e9fbefb9d1fbba
kiyoshion <kiyoshion@gmail.com>
2022-10-22 03:44:18 +0900

    Fixed sections to contents

--- src/components/materials/CreateMaterial.js
+++ src/components/materials/CreateMaterial.js
@@ -1,23 +1,23 @@
 import axios from "../../lib/axios"
 import { useState, useCallback, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import utilStyles from '../../styles/util.module.scss'
 import { useDropzone } from 'react-dropzone'
-import MaterialStyles from './Material.module.scss'
-import { type } from "@testing-library/user-event/dist/type"
 import { Types } from '../../Types'
+import { CONSTS } from '../../Consts'
+import utilStyles from '../../styles/util.module.scss'
+import MaterialStyles from './Material.module.scss'
 
 export default function CreateItem() {
   const [ title, setTitle ] = useState("")
-  const [ sections, setSections ] = useState([
-    { title: "シーズン", level: 0 },
-    { title: "エピソード", level: 1 },
+  const [ contents, setContents ] = useState([
+    { title: "1", order: 1 }
   ])
+  const [ chapters, setChapters ] = useState([
+    { title: "1", order: 1, parentOrder: 1 }
+  ])
+  const [ parentOrder, setParentOrder ] = useState(1)
   const [ poster, setPoster ] = useState(null)
   const [ types, setTypes ] = useState([])
-  const [ parentTypes, setParentTypes ] = useState([])
-  const [ childrenTypes, setChildrenTypes ] = useState([])
-  const updateMaterialURL = '/api/materials'
   const getTypesURL = '/api/types'
   const navigate = useNavigate();
 
@@ -29,10 +29,11 @@ export default function CreateItem() {
         params.append("poster", poster)
         params.append("title", title)
         params.append("user_id", res.data.id)
-        params.append("sections", JSON.stringify(sections))
+        params.append("contents", JSON.stringify(contents))
+        params.append("chapters", JSON.stringify(chapters))
         console.log(poster)
         axios
-          .post(updateMaterialURL, params, { headers: {
+          .post(CONSTS.GET_MATERIALS_URL, params, { headers: {
             'Content-Type': 'multipart/form-data'
           }})
           .then((res) => {
@@ -41,17 +42,28 @@ export default function CreateItem() {
       })
   }
 
-  const addSection = () => {
-    setSections(() => [...sections, { title: "", level: 0 }])
+  const addContent = () => {
+    setContents(() => [...contents, { title: contents.length + 1, order: contents.length + 1 }])
+    setParentOrder(contents.length + 1)
   }
 
-  const addSectionChildren = () => {
-    setSections(() => [...sections, { title: "", level: 1 }])
+  const addChapter = (parentOrder) => {
+    setChapters(() => [...chapters, { title: chapters.length + 1, order: chapters.length + 1, parentOrder: parentOrder }])
   }
 
-  const updateSections = (index, newData) => {
-    setSections(
-      sections.map((section, i) => (i === index ? { title: newData, level: section.level }: section))
+  const updateContents = (index, newData, order) => {
+    setContents(
+      contents.map((content, i) =>
+        (i === index ? { title: newData, order: order } : content)
+      )
+    )
+  }
+
+  const updateChapters = (index, newData, order, parentOrder) => {
+    setChapters(
+      chapters.map((chapter, i) =>
+        (i === index ? { title: newData, order: order, parentOrder: parentOrder } : chapter)
+      )
     )
   }
 
@@ -64,7 +76,6 @@ export default function CreateItem() {
       reader.onabort = () => console.log('file reading was aborted')
       reader.onerror = () => console.log('file reading has failed')
       reader.onload = () => {
-      // Do whatever you want with the file contents
         const binaryStr = reader.result
         console.log(binaryStr)
       }
@@ -87,35 +98,20 @@ export default function CreateItem() {
   });
 
   const thumb = {
-  display: 'inline-flex',
-  borderRadius: 2,
-  border: '1px solid #eaeaea',
-  marginBottom: 8,
-  marginRight: 8,
-  width: 100,
-  height: 100,
-  padding: 4,
-  boxSizing: 'border-box'
-};
+    display: 'inline-flex', borderRadius: 2, border: '1px solid #eaeaea', marginBottom: 8, marginRight: 8, width: 100, height: 100, padding: 4, boxSizing: 'border-box'
+  };
 
-const thumbInner = {
-  display: 'flex',
-  minWidth: 0,
-  overflow: 'hidden'
-};
+  const thumbInner = {
+    display: 'flex', minWidth: 0, overflow: 'hidden'
+  };
 
-const img = {
-  display: 'block',
-  width: 'auto',
-  height: '100%'
-};
+  const img = {
+    display: 'block', width: 'auto', height: '100%'
+  };
 
-const thumbsContainer = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  marginTop: 16
-};
+  const thumbsContainer = {
+    display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginTop: 16
+  };
 
   const thumbs = files.map(file => (
     <div style={thumb} key={file.name}>
@@ -124,7 +120,6 @@ const thumbsContainer = {
         alt=""
           src={file.preview}
           style={img}
-          // Revoke data uri after image is loaded
           onLoad={() => { URL.revokeObjectURL(file.preview) }}
         />
       </div>
@@ -167,45 +162,20 @@ const thumbsContainer = {
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             {Types.ja.map((type) => {
               return (
-                // <div style={{ display: 'flex', alignItems: 'center' }}>
                 <div key={type.id}>
                   <span>{type.name}</span>
                   <div style={{ display: 'flex' }}>
-                  { type.children.map((type) => {
-                    return (
+                    {type.children.map((type) => {
+                      return (
                         <p key={type.id}>{type.name}</p>
-                        )
-                      }) }
-                      </div>
+                      )
+                    })}
+                  </div>
                 </div>
               )
             })}
           </div>
         </div>
-        {/* <div style={{ display: 'flex' }}>
-          <div style={{ display: 'flex', flexDirection: 'column'}}>
-            { types[0].map((type) => {
-              return (
-                <span>{type.name}</span>
-              )
-            }) }
-          </div>
-          <div>
-            <div style={{ display: 'flex' }}>
-            </div>
-          </div>
-        </div> */}
-        {/* <div>
-          {types.map((type) => {
-            return (
-              type.parent_id !== 0 ? (
-                <div key={type.id}>{type.name}</div>
-                ) : (
-                <div key={type.id}></div>
-              )
-            )
-          })}
-        </div> */}
         <div className={MaterialStyles.posterContainer}>
           <div className={MaterialStyles.inputFileContainer}>
             <div {...getRootProps()}>
@@ -218,18 +188,37 @@ const thumbsContainer = {
           </aside>
         </div>
         <div>
-          {sections.map((section, index) =>
-            ( <input
-                type="text"
-                id={index}
-                key={index}
-                value={section.title}
-                onChange={(e) => updateSections(index, e.target.value )}
-              />)
-          )}
+          {contents.map((content, i) => {
+            return (
+              <div key={i}>
+                <input
+                  type="text"
+                  id={i}
+                  value={content.title}
+                  onChange={(e) => updateContents(i, e.target.value, content.order)}
+                  style={{marginBottom:'.5rem'}}
+                />
+                {chapters.map((chapter, j) => {
+                  if (chapter.parentOrder === content.order) {
+                    return (
+                      <div key={j}
+                      style={{ margin: '0 0 .5rem 1rem' }}>
+                        <input
+                          type="text"
+                          id={j}
+                          value={chapter.title}
+                          onChange={(e) => updateChapters(j, e.target.value, chapter.order, chapter.parentOrder)}
+                        />
+                      </div>
+                    )
+                  }
+                })}
+              </div>
+            )
+          })}
         </div>
-        <button onClick={addSection}>シーズンを追加</button>
-        <button onClick={addSectionChildren}>エピソードを追加</button>
+        <button onClick={addContent}>コンテンツを追加</button>
+        <button onClick={() => addChapter(parentOrder)}>チャプターを追加</button>
         <div>
           <button
             className={`${utilStyles.btn} ${utilStyles.btn_black}`}
